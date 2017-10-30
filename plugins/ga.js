13dc84f6020c8baf87fb0d55c8e74800bce10b52
Cris <corgidisco@gmail.com>
2017-10-31 00:56:06 +0900

    fix ga

--- plugins/ga.js
+++ plugins/ga.js
@@ -2,13 +2,15 @@
 /* eslint-disable */
 export default ({app}) => {
   if (process.env.NODE_ENV !== 'production') return
-  
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+  gtag('config', 'UA-108846016-1')
+
   app.router.afterEach((to, from) => {
-    window.gtag('config', 'UA-108846016-1', {
+    gtag('config', 'UA-108846016-1', {
       page_path: location.href,
-    });
+    })
   })
 }
